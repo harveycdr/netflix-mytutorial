2fec8cb3498529d510bd06ac40142f2767467159
harveycdr <harvey.hau4502@gmail.com>
2024-02-09 16:18:59 +0700

    2nd commit

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import type { PrismaClient } from '@prisma/client';
+import type { MongoClient } from 'mongodb';
 
 declare global {
   namespace globalThis {
     var prismadb: PrismaClient
   }
 }
-
